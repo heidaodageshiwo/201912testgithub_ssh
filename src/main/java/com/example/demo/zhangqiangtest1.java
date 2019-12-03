6dce69bc4759b774705e23a7aadee2ef56dffd0d
zhangqiang <heidaodageshiwo@163.com>
2019-12-03 23:18:35 +0800

    testssh

---
+++ src/main/java/com/example/demo/zhangqiangtest1.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: demo
+ * @Package: com.example.demo
+ * @ClassName: zhangqiangtest1
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2019-12-03 23:18
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2019-12-03 23:18
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * *
+ */
+public class zhangqiangtest1 {
+    //zhangqiangtest1
+    //zhangqiangtest1
+    //zhangqiangtest1
+    //zhangqiangtest1
+    //zhangqiangtest1
+}
