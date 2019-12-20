bf557410850bb7dba2e3f2969e096183a21b038b
zhangqiang <heidaodageshiwo@163.com>
2019-12-20 09:14:49 +0800

    mastertest

---
+++ src/main/java/com/example/demo/master.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: demo
+ * @Package: com.example.demo
+ * @ClassName: master
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2019-12-20 9:13
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2019-12-20 9:13
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * *
+ */
+public class master {
+    public static void main(String[] args) {
+        System.out.println("master test");
+    }
+}
